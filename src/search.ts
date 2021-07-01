226ff417ef51687e6318cc58f3496625f87e7a95
Maxime <maxime@fbsc.fr>
2021-07-01 15:07:20 +0200

    feature: search is no longer sensitive to typo

--- src/search.ts
+++ src/search.ts
@@ -2,64 +2,102 @@ import mariadb from "mariadb";
 import Market from "./market";
 
 export default class Search {
-    static db: mariadb.Pool;
+	static db: mariadb.Pool;
 
-    static async init(db: mariadb.Pool) {
-        this.db = db;
-    }
+	static async init(db: mariadb.Pool) {
+		this.db = db;
 
-    static async search(query?: string) {
-        if (!query) throw "Query required";
+		console.log(await this.search("sharpnes"));
+	}
 
-        query = query.toLowerCase();
+	static async search(query?: string) {
+		if (!query) throw "Query required";
 
-        let tags = query.split(" ");
+		query = query.toLowerCase();
 
-        let items = await Market.getAllItems();
+		let tags = query.split(" ");
 
-        let triables_items: ItemTriable[] = [];
+		let items = await Market.getAllItems();
 
-        items.forEach(item => {
-            triables_items.push(new ItemTriable(item));
-        });
+		let triables_items: ItemTriable[] = [];
 
-        let sorted = this.recursiveSearch(tags, triables_items);
+		items.forEach(item => {
+			triables_items.push(new ItemTriable(item));
+		});
 
-        return sorted.map(item => item.item.id);
-    }
+		let sorted = this.recursiveTagsSearch(tags, triables_items, true);
 
-    private static recursiveSearch(tags: string[], items: ItemTriable[], recursiveCount = 0): ItemTriable[] {
-        if (recursiveCount > 5) {
-            console.warn("max search recursive count reached");
-            return items;
-        }
+        console.log(sorted.map(item => item.item.name));
+        
 
-        tags.forEach(tag => {
-            items.forEach(item => {
-                if (item.item.tags.includes(tag)) item.weight += 1;
-            });
-        });
+		return sorted.map(item => item.item.id);
+	}
 
-        let notFound = items.filter(item => item.weight === 0);
-        items = items.filter(item => item.weight > 0);
+	private static recursiveTagsSearch(tags: string[], items: ItemTriable[], strict = false, recursiveCount = 0): ItemTriable[] {
+		if (recursiveCount > 2) {
+			console.warn("max search recursive count reached");
+			return items;
+		}
 
-        if (items.length === 0) return notFound;
+		tags.forEach(tag => {
+			items.forEach(item => {
+				item.item.tags.forEach(item_tag => {
+					let leven = this.levenshtein(tag, item_tag);
+					if (leven < (strict ? 1 : 4)) item.weight += 1 / leven + 1;
+				});
+			});
+		});
 
-        let sorted = items.sort((a, b) => b.weight - a.weight);
+		let notFound = items.filter(item => item.weight === 0);
+		items = items.filter(item => item.weight > 0);
 
-        if (notFound.length > 0)
-            sorted = sorted.concat(this.recursiveSearch(sorted[0].item.tags, notFound, recursiveCount + 1));
+		if (items.length === 0) return notFound;
 
-        return sorted;
-    }
+		let sorted = items.sort((a, b) => b.weight - a.weight);
+
+		if (notFound.length > 0) sorted = sorted.concat(this.recursiveTagsSearch(sorted[0].item.tags, notFound, true, recursiveCount + 1));
+
+		return sorted;
+	}
+
+	private static levenshtein(str1: string, str2: string) {
+		// console.time("leven");
+
+		let D: number[][] = [];
+
+		let cost: number;
+
+		for (let i = 0; i <= str1.length; i++) {
+			D[i] = [];
+			for (let j = 0; j <= str2.length; j++) {
+				if (i * j === 0) {
+					D[i][0] = i;
+					D[0][j] = j;
+				} else D[i][j] = 0;
+			}
+		}
+
+		for (let i = 1; i <= str1.length; i++) {
+			for (let j = 1; j <= str2.length; j++) {
+				if (str1[i - 1] === str2[j - 1]) cost = 0;
+				else cost = 1;
+
+				D[i][j] = Math.min(D[i - 1][j] + 1, D[i][j - 1] + 1, D[i - 1][j - 1] + cost);
+			}
+		}
+		// console.timeEnd("leven");
+		// console.log(str2);
+
+		return D[str1.length][str2.length];
+	}
 }
 
 class ItemTriable {
-    readonly item: Item;
-    weight: number;
+	readonly item: Item;
+	weight: number;
 
-    constructor(item: Item, weight: number = 0) {
-        this.item = item;
-        this.weight = weight;
-    }
+	constructor(item: Item, weight: number = 0) {
+		this.item = item;
+		this.weight = weight;
+	}
 }
